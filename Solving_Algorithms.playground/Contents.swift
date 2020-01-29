c7599254f69f42d6e901e765fcf3f2230a884f17
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2020-01-29 12:32:24 -0800

    Adding functions

--- Solving_Algorithms.playground/Contents.swift
+++ Solving_Algorithms.playground/Contents.swift
@@ -44,11 +44,67 @@ func binarySearch(array : [Int], target : Int) -> Bool
 }
 var numbers = [2,8,16,22,24,24,29,31,33,37,40]
 
+binarySearch(array: numbers, target: 41)
 
-if binarySearch(array: numbers, target: 24) == true
+class Node
 {
-    print("Found in this array")
-}else
-{
-    print("Could not find in this array")
+    var value : Int
+    var next : Node?
+    init(value : Int, next : Node?)
+    {
+        self.value = value
+        self.next = next
+    }
 }
+
+class LinkedList
+{
+    var head : Node?
+    
+    func insertNode(value : Int)
+    {
+        if head == nil
+        {
+            head = Node(value: value, next: nil)
+            print("Successfully inserted node")
+            return
+        }
+        var currentNode = head
+        while currentNode?.next != nil {
+            currentNode = currentNode?.next
+        }
+        currentNode?.next = Node(value: value, next: nil)
+    }
+    func displayList()
+    {
+        var currentNode = head
+        while currentNode != nil {
+            print(currentNode!.value)
+            currentNode = currentNode?.next
+        }
+    }
+    func deleteNode(value : Int)
+    {
+        if head?.value == value
+        {
+            head = head?.next
+        }
+        var currentNode = head
+        var previousNode : Node?
+        while currentNode?.next != nil && currentNode?.value != value {
+            previousNode = currentNode
+            currentNode = currentNode?.next
+        }
+        previousNode?.next = currentNode?.next
+    }
+}
+let linkedList = LinkedList()
+var index = 0
+while index < 10 {
+    linkedList.insertNode(value: index)
+    index = index + 1
+}
+linkedList.displayList()
+linkedList.deleteNode(value: Int.random(in: 0...10))
+print("After deletion")
+linkedList.displayList()
