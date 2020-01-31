792630fcd51440e801e03fd5bd324747e08f57cd
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2020-01-30 22:32:56 -0800

    isUnique

--- Reverse_String.playground/Contents.swift
+++ Reverse_String.playground/Contents.swift
@@ -8,7 +8,30 @@ func revString(word : String)
         wordArray.append(char)
         
     }
-    print(wordArray)
+    var index = 0
+    while wordArray.count != 0 {
+        wordArray.removeLast()
+        index = index + 1
+    }
 }
 
 revString(word: "hello")
+
+
+func isUnique(word : String) -> Bool
+{
+    var myArray : [Character] = []
+    for char in word
+    {
+        myArray.append(char)
+    }
+    myArray.sort()
+    for char in myArray
+    {
+        if myArray[char] == myArray[char + 1]
+        {
+            return false
+        }
+    }
+    return true
+}
