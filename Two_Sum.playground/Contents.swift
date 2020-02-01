87ce6a35faeed2dbfcb464b8d5581c3e6f6030b2
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2020-01-31 16:13:26 -0800

    Fixed sorting method in two sum

--- Two_Sum.playground/Contents.swift
+++ Two_Sum.playground/Contents.swift
@@ -20,17 +20,47 @@ func twoSum(numbers: [Int], sum : Int) -> [Int]?
     return nil
 }
 
-let numbers = [3,8,12,9,2,4]
-let result = twoSum(numbers: numbers, sum: 16)
 var index = 0
 var arrayOfNumbers : [Int] = []
-while index < 10 {
-    let randomInt = Int.random(in: 0...25)
+while index < 10
+{
+    let randomInt = Int.random(in: 0...40)
     arrayOfNumbers.append(randomInt)
     index = index + 1
 }
+
 print(arrayOfNumbers)
+let result = twoSum(numbers: arrayOfNumbers, sum: 30)
 
-let newResult = twoSum(numbers: arrayOfNumbers, sum: 25)
+func mergeSort(array : [Int]) -> [Int]
+{
+    guard array.count > 1 else {
+        return array
+    }
+    let leftArray = Array(array[0..<array.count/2])
+    let rightArray = Array(array[array.count/2..<array.count])
+    return merge(left: mergeSort(array: leftArray), right: mergeSort(array: rightArray))
+}
 
-print(newResult ?? "")
+func merge(left : [Int], right : [Int]) -> [Int]
+{
+    var mergedArray : [Int] = []
+    var left = left
+    var right = right
+    while left.count > 0 && right.count > 0 {
+        if left.first! < right.first!
+        {
+            mergedArray.append(left.removeFirst())
+        }else
+        {
+            mergedArray.append(right.removeFirst())
+        }
+    }
+    return mergedArray + left + right
+}
+
+
+let numbers = [9,2,3,8,1,7]
+
+
+print(mergeSort(array: arrayOfNumbers))
